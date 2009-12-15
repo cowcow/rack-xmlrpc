e3f690793edcdd07b7065a6526dd22494289a994
cowcow <cow.cw1@gmail.com>
2009-12-16 01:19:32 +0900

    rack-xmlrpc 一応動作する状況

---
+++ rack-xmlrpc.rb
@@ -0,0 +1,85 @@
+
+require 'xmlrpc/server'
+require 'rack/request'
+require 'rack/response'
+
+module Rack
+	class XMLRPC < XMLRPC::BasicServer
+
+		class XMLRPCError < StandardError
+			attr_reader :status
+			attr_reader :message
+
+			def initialize( status, message )
+				super()
+				@status = status
+				@message = message
+			end
+		end
+
+		def call(env)
+			req = Request.new(env)
+			res = Response.new()
+			res['Content-Type'] = 'text/xml; charset=utf-8'
+
+			unless( req.post? )
+				raise XMLRPCError.new( 405, "Method Not Allowed" )
+			end
+
+			unless( parse_content_type(req.content_type).first == "text/xml" )
+				raise XMLRPCError.new( 400, "Bad Request" )
+			end
+
+			length = req.content_length().to_i
+
+			unless( length > 0 )
+				raise XMLRPCError.new( 411, "Length Required" )
+			end
+
+			data = req.env["rack.input"].read
+
+			if( data.nil? or data.size != length )
+				raise XMLRPCError.new( 400, "Bad Request" )
+			end
+
+			res.write( process( data ) )
+			res.finish
+		rescue XMLRPCError => e
+			res.status = e.status
+			res.write( http_error( e.status, e.message ) )
+			res.finish
+		end
+
+		def http_error( status, message )
+			err = "#{status} #{message}"
+			msg = <<-"MSGEND"
+			<html>
+			<head>
+				<title>#{err}</title>
+			</head>
+			<body>
+				<h1>#{err}</h1>
+				<p>Unexpected error occured while processing XML-RPC request!</p>
+			</body>
+			</html>
+			MSGEND
+		end
+
+	end
+end
+
+if $0 == __FILE__
+	require 'rack'
+	require 'rack/showexceptions'
+
+	s = Rack::XMLRPC.new
+	s.add_handler("xmlrpc") { |a,b|
+		a + b
+	}
+
+	Rack::Handler::WEBRick.run(
+		Rack::ShowExceptions.new(
+			Rack::Lint.new( s )
+		),
+		:Port => 9292 )
+end
